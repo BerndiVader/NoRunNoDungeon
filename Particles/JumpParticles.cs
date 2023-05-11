f3d08fa72651f381f9dd2c3cc47eddc229f9b450
BerndiVader <kasermandel@gmail.com>
2023-05-11 11:05:38 +0200

    jump particles track player fliph

---
+++ Particles/JumpParticles.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class JumpParticles : CPUParticles2D
+{
+    AnimatedTexture animTex;
+
+    public override void _Ready()
+    {
+        animTex=(AnimatedTexture)Texture;
+        animTex.CurrentFrame=0;
+    }
+
+    public override void _Process(float delta)
+    {
+        animTex.CurrentFrame=Player.instance.animationController.FlipH?1:0;
+    }
+
+}
