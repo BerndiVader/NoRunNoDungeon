747dbef0c5d4ea0f550d028930afdc69533b509c
BerndiVader <kasermandel@gmail.com>
2023-05-12 19:35:55 +0200

    added Fairy enemy

---
+++ Enemies/Fairy.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public class Fairy : KinematicMonster
+{
+    private float passedTime;
+    private Vector2 defaultPos;
+    [Export]private Vector2 SinCosSpeed=new Vector2(5f,3.5f);
+    [Export]private Vector2 FloatRange=new Vector2(5f,5f);
+
+    public override void _Ready()
+    {
+        defaultPos=new Vector2(Position);
+        passedTime=0f;
+        SetProcess(true);
+        SetPhysicsProcess(false);
+        base._Ready();
+        state=STATE.unknown;
+        EmitSignal(STATE.idle.ToString());
+    }
+
+    public override void _Process(float delta)
+    {
+        goal(delta);
+    }
+
+    protected override void idle(float delta)
+    {
+        fly(delta);
+    }
+
+    protected override void onIdle()
+    {
+        onDelay=false;
+        if(state!=STATE.idle)
+        {
+            lastState=state;
+            state=STATE.idle;
+            animationController.Play("idle");
+            goal=idle;
+            defaultPos=new Vector2(Position);
+        }
+    }
+
+    private void fly(float delta)
+    {
+        passedTime+=delta;
+        Position=new Vector2(defaultPos.x+(FloatRange.x*Mathf.Sin(passedTime*SinCosSpeed.x)),defaultPos.y+(FloatRange.y*Mathf.Cos(passedTime*SinCosSpeed.y)));
+    }
+
+}
