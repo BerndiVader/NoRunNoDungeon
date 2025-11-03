91420152f5b007d34b0be88c7fc194a71a059e55
BerndiVader <kasermandel@gmail.com>
2025-11-03 10:17:48 +0100

    MovingPlatforms logic now dont get stuck on float overflow if moving.
    Added Bonus collision to WalkingTree enemy.

--- platforms/MovingPlatform.cs
+++ platforms/MovingPlatform.cs
@@ -4,31 +4,29 @@ using System;
 public class MovingPlatform : Platform
 {
 
-    [Export] private Vector2 Direction=new Vector2(0f,-1f);
+    [Export] private Vector2 Direction=Vector2.Up;
     [Export] private float Speed=20f;
     [Export] private float MaxSpeed=20f;
-    [Export] private int Length=10;
-    [Export] private float LerpFactor=4f;
+    [Export] private int Length=2;
     [Export] private bool Linear=true;
-    [Export] private float Shrink=0f;
 
-    private int maxDistance;
+    private float maxDistance;
     private float speed;
-    private Vector2 maxPosition;
 
     public override void _Ready()
     {
         base._Ready();
-        maxDistance=Length*16;
-        speed=Speed;
+
+        maxDistance = Length * 16;
+        speed = Speed;
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        lastPosition=Position;
-        float distance=Position.DistanceTo(startPosition);
+        lastPosition = Position;
+        float distance = Position.DistanceTo(startPosition);
 
-        if(distance>=maxDistance)
+        if(((int)distance)>maxDistance)
         {
             Direction*=-1;
         }
