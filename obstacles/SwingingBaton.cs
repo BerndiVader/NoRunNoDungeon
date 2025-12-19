e8e7b05ccb3abe4d1753b43c2fd02e614352f8f3
BerndiVader <kasermandel@gmail.com>
2025-12-19 19:50:18 +0100

    Reworked swing logic and added different swing types.

--- obstacles/SwingingBaton.cs
+++ obstacles/SwingingBaton.cs
@@ -1,25 +1,89 @@
 using Godot;
 using System;
+using System.Collections;
 
-public class SwingingBaton : Area2D
+public class SwingingBaton : Area2D,ISwitchable
 {
-    [Export] private float minSpeed=0.01f;
-    [Export] private float maxSpeed=0.09f;
-    [Export] private int maxRotation=90;
+    private enum SWINGTYPE
+    {
+        LINEAR,
+        SINUS,
+        SINUS_DAMPING
+    }
+    private enum MODE
+    {
+        NORMAL,
+        SWITCH,
+        DISTANCE
+    }
+
+    [Export] private float minSpeed=0.2f;
+    [Export] private float maxSpeed=4f;
+    [Export] private float maxRotation=90f;
+    [Export] private float SinusDamping=0.995f;
+    [Export] private MODE mode=MODE.NORMAL;
+    [Export] private SWINGTYPE type=SWINGTYPE.LINEAR;
+    [Export] private string switchID="";
+    [Export] private bool oneShoot=false;
+    [Export] private float activationRange=100f;
     
     private Vector2 rotateTo,rot;
+    private float linMinSpeed;
+    private float linMaxSpeed;
+    private float startRotation;
+    private float sinSwingTime;
+    private float sinAmplitude;
+    private float sinSwingPhase;
+    private float sinFrequency;
+
+    private delegate void Goal(float delta);
+    private Goal goal;
 
     public override void _Ready()
     {
-        AddToGroup(GROUPS.OBSTACLES.ToString());
-
         VisibilityNotifier2D notifier2D=new VisibilityNotifier2D();
         notifier2D.Connect("screen_exited",World.instance,nameof(World.OnObjectExitedScreen),new Godot.Collections.Array(this));
         AddChild(notifier2D);
 
-        rotateTo=new Vector2(Mathf.Deg2Rad(90),0);
-        rot=new Vector2(Rotation,0);
+        AddToGroup(GROUPS.OBSTACLES.ToString());
+
+        switch(mode)
+        {
+            case MODE.NORMAL:
+            break;
+            case MODE.SWITCH:
+                AddToGroup(GROUPS.SWITCHABLES.ToString());
+                SetPhysicsProcess(false);
+                break;
+            case MODE.DISTANCE:
+            break;
+        }
         
+        sinAmplitude=Mathf.Deg2Rad(maxRotation);
+        sinFrequency=Mathf.Lerp(minSpeed,maxSpeed,0.5f);
+
+        linMinSpeed=sinAmplitude*minSpeed;
+        linMaxSpeed=sinAmplitude*maxSpeed;
+
+        switch(type)
+        {
+            case SWINGTYPE.LINEAR:
+                rotateTo=new Vector2(Mathf.Deg2Rad(maxRotation),0f);
+                rot=new Vector2(Rotation,0f);
+                goal=Linear;
+                break;
+            case SWINGTYPE.SINUS:
+                sinSwingTime=0f;
+                sinSwingPhase=(sinAmplitude!=0f)?Mathf.Asin(Mathf.Clamp(Rotation/sinAmplitude,-1f,1f)):0f;
+                goal=Sinus;
+                break;
+            case SWINGTYPE.SINUS_DAMPING:
+                sinSwingTime=0f;
+                sinSwingPhase=(sinAmplitude!=0f)?Mathf.Asin(Mathf.Clamp(Rotation/sinAmplitude,-1f,1f)):0f;
+                goal=SinDamping;            
+                break;
+        }
+
         Connect("body_entered",this,nameof(OnBodyEntered));
     }
 
@@ -33,21 +97,58 @@ public class SwingingBaton : Area2D
 
     public override void _PhysicsProcess(float delta)
     {
-        float speed=rot.x;
-        rot=rot.LinearInterpolate(rotateTo,delta*3);
-        speed=rot.x-speed;
-        speed=MathUtils.MinMax(minSpeed,maxSpeed,Math.Abs(speed))*Math.Sign(speed);
-        Rotate(speed);
-
-        if(RotationDegrees>maxRotation-1)
-        {
-            rotateTo=new Vector2(Mathf.Deg2Rad(maxRotation*-1),0);
-        }
-        else if(RotationDegrees<(maxRotation-1)*-1)
-        {
-            rotateTo=new Vector2(Mathf.Deg2Rad(maxRotation),0);
-        }
-
+        goal(delta);
     }
 
+    private void Linear(float delta)
+    {
+        float speed=rot.x;
+        rot=rot.LinearInterpolate(rotateTo,delta*3f);
+        speed=rot.x-speed;
+        speed=MathUtils.MinMax(linMinSpeed,linMaxSpeed,Math.Abs(speed))*Math.Sign(speed)*delta*8f;
+        Rotate(speed);
+
+        if(RotationDegrees>maxRotation-1f)
+        {
+            rotateTo=new Vector2(Mathf.Deg2Rad(maxRotation*-1f),0f);
+            if(mode==MODE.SWITCH||oneShoot)
+            {
+                SetPhysicsProcess(false);
+            }
+        }
+        else if(RotationDegrees<(maxRotation-1f)*-1f)
+        {
+            rotateTo=new Vector2(Mathf.Deg2Rad(maxRotation),0f);
+            if(mode==MODE.SWITCH||oneShoot)
+            {
+                SetPhysicsProcess(false);
+            }
+        }
+    }
+
+    private void Sinus(float delta)
+    {
+        sinSwingTime+=delta;
+        Rotation=Mathf.Sin(sinSwingTime*sinFrequency+sinSwingPhase)*sinAmplitude;
+    }
+
+    private void SinDamping(float delta)
+    {
+        sinSwingTime+=delta;
+        sinAmplitude*=SinusDamping;
+        Rotation=Mathf.Sin(sinSwingTime*sinFrequency+sinSwingPhase)*sinAmplitude;
+
+        if(Mathf.Abs(sinAmplitude)<0.01f&&Mathf.Abs(RotationDegrees)<2f)
+        {
+            SetPhysicsProcess(false);
+        }
+    }
+
+    public void SwitchCall(string id)
+    {
+        if(switchID==id&&!oneShoot&&!IsPhysicsProcessing())
+        {
+            SetPhysicsProcess(true);
+        }
+    }
 }
