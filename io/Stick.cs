ff3053d6a130b551074c86b7ddece33866cf12cc
BerndiVader <kasermandel@gmail.com>
2025-12-23 10:42:07 +0100

    Avoid InputEvent object spikes if inputevent is fired.

--- io/Stick.cs
+++ io/Stick.cs
@@ -9,7 +9,8 @@ public class Stick : TouchScreenButton
     private int onGoing=-1;
     private float returnAccel=60f;
     private float threshold=10f;
-    private Vector2 hidePosition=new Vector2(-200f,-200f);
+    private Vector2 hidePosition=new Vector2(-100f,-100f);
+    private bool visible;
     private Vector2 approxPosition=new Vector2(-16f,-16f);
     public bool useAccelerometer;
 
@@ -19,6 +20,7 @@ public class Stick : TouchScreenButton
 
         touch=GetParent<Touch>();
         touch.Position=hidePosition;
+        visible=false;
         touch.oPosition=Position;
 
         useAccelerometer=false;
@@ -36,10 +38,11 @@ public class Stick : TouchScreenButton
         {
             Vector2 diffPos=Vector2.Zero-rad-Position;
             Position+=diffPos*returnAccel*delta;
-            if(Position.IsEqualApprox(approxPosition)) 
+            if(visible&&Position.IsEqualApprox(approxPosition)) 
             {
                 touch.Position=hidePosition;
                 touch.oPosition=hidePosition;
+                visible=false;
                 onGoing--;
             }
         }
@@ -47,51 +50,55 @@ public class Stick : TouchScreenButton
 
     public override void _Input(InputEvent @event)
     {
-        if(@event is InputEventScreenDrag || (@event is InputEventScreenTouch && @event.IsPressed()))
+        using(@event)
         {
-            InputEventScreenDrag d=@event as InputEventScreenDrag;
-            InputEventScreenTouch t=@event as InputEventScreenTouch;
-
-            int index;
-            Vector2 position;
-
-            if(d!=null)
+            if(@event is InputEventScreenDrag || (@event is InputEventScreenTouch && @event.IsPressed()))
             {
-                position=d.Position;
-                index=d.Index;
-            }
-            else
-            {
-                position=t.Position;
-                index=t.Index;
-                
-                if(@t.IsPressed()&&position.x<256f)
+                InputEventScreenDrag d=@event as InputEventScreenDrag;
+                InputEventScreenTouch t=@event as InputEventScreenTouch;
+
+                int index;
+                Vector2 position;
+
+                if(d!=null)
                 {
-                    touch.Position=position;
-                    touch.oPosition=position;                
+                    position=d.Position;
+                    index=d.Index;
                 }
-            }
-
-            float distCenter=(position-touch.Position).Length();
-
-            if(distCenter<=boundary||index==onGoing)
-            {
-                Position=position-touch.Position-rad;
-                if(GetButtonPosition().Length()>boundary)
+                else
                 {
-                    Position=GetButtonPosition().Normalized()*boundary-rad;
+                    position=t.Position;
+                    index=t.Index;
+                    
+                    if(@t.IsPressed()&&position.x<256f)
+                    {
+                        touch.Position=position;
+                        touch.oPosition=position;
+                        visible=true;
+                    }
                 }
-                onGoing=index;
+
+                float distCenter=(position-touch.Position).Length();
+
+                if(distCenter<=boundary||index==onGoing)
+                {
+                    Position=position-touch.Position-rad;
+                    if(GetButtonPosition().Length()>boundary)
+                    {
+                        Position=GetButtonPosition().Normalized()*boundary-rad;
+                    }
+                    onGoing=index;
+                }
+
             }
 
-        }
-
-        if(@event is InputEventScreenTouch && !@event.IsPressed())
-        {
-            InputEventScreenTouch eventScreenTouch=@event as InputEventScreenTouch;
-            if(eventScreenTouch.Index==onGoing)
+            if(@event is InputEventScreenTouch && !@event.IsPressed())
             {
-                onGoing=-1;
+                InputEventScreenTouch eventScreenTouch=@event as InputEventScreenTouch;
+                if(eventScreenTouch.Index==onGoing)
+                {
+                    onGoing=-1;
+                }
             }
         }
     }
