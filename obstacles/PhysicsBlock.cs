038b43a8c7b0da3dc76594178d5f774064758f81
BerndiVader <kasermandel@gmail.com>
2026-01-24 20:09:05 +0100

    Introduced PhysicBlocks and destoryable PhysicBlocks.

---
+++ obstacles/PhysicsBlock.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public class PhysicsBlock : PhysicsObject
+{
+    [Export] protected float STOP_FORCE=1300f;
+
+    private StaticBody2D staticBody;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        staticBody=GetNode<StaticBody2D>(nameof(StaticBody2D));
+
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Navigation(delta);
+    }
+
+    private void Navigation(float delta)
+    {
+        velocity+=GRAVITY*delta;
+        velocity=MoveAndSlideWithSnap(velocity,Vector2.Zero,new Vector2(8f,8f),true,4,0f,true);
+        
+        int slides=GetSlideCount();
+        if(slides>0)
+        {
+            for(int i=0;i<slides;i++)
+            {
+                var collision=GetSlideCollision(i);
+                if(collision.Collider is Platform platform&&collision.Normal==Vector2.Up)
+                {
+                    velocity.x=platform.CurrentSpeed.x;
+                }
+                else
+                {
+                    velocity=StopX(velocity,delta);
+                }
+            }    
+        }
+        else
+        {
+            velocity=StopX(velocity,delta);
+        }
+
+    }
+
+    private Vector2 StopX(Vector2 velocity,float delta)
+    {
+        float xLength=Mathf.Abs(velocity.x)-(STOP_FORCE*delta);
+        if(xLength<0f) {
+            xLength=0f;
+        }
+        velocity.x=xLength*Mathf.Sign(velocity.x);
+        return velocity;
+    }
+}
