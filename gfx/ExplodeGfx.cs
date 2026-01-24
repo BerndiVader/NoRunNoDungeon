551e70c2ec34a036ae4cb4572c2e7d67e27bc576
BerndiVader <kasermandel@gmail.com>
2026-01-24 20:08:11 +0100

    Added use particles option.

--- gfx/ExplodeGfx.cs
+++ gfx/ExplodeGfx.cs
@@ -3,12 +3,16 @@ using System;
 
 public class ExplodeGfx : AnimatedSprite
 {
+    public bool useParticles=true;
     protected int animationLength;
     public override void _Ready()
     {
-        ExplodeParticles particles=ResourceUtils.particles[(int)PARTICLES.EXPLODE].Instance<ExplodeParticles>();
-        particles.Position=Position;
-        World.level.AddChild(particles);
+        if(useParticles)
+        {
+            ExplodeParticles particles=ResourceUtils.particles[(int)PARTICLES.EXPLODE].Instance<ExplodeParticles>();
+            particles.Position=Position;
+            World.level.AddChild(particles);
+        }
         ZIndex=4;
         animationLength=Frames.GetFrameCount(Animation)-1;
         Play();
