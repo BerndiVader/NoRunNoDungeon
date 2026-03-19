7d5a5df06ed2ca2d0e1727020866e9ecc609a3c4
BerndiVader <kasermandel@gmail.com>
2026-03-19 18:15:47 +0100

    Minor adjustments.

--- particles/JumpParticles.cs
+++ particles/JumpParticles.cs
@@ -7,7 +7,7 @@ public class JumpParticles : CPUParticles2D
 
     public override void _Ready()
     {
-        SetPhysicsProcess(false);
+        SetPhysicsProcess(true);
         SetProcess(false);
         SetProcessInput(false);
 
@@ -15,9 +15,20 @@ public class JumpParticles : CPUParticles2D
         animTex.CurrentFrame=0;
     }
 
+    public override void _PhysicsProcess(float delta)
+    {
+        bool flip=Player.instance.AnimationController.FlipH;
+        int frame=flip?1:0;
+        if(animTex.CurrentFrame!=frame)
+        {
+            animTex.CurrentFrame=frame;
+        }
+    }
+
     public void Start(bool flipped)
     {
         animTex.CurrentFrame=flipped?1:0;
+        Emitting=false;
         Emitting=true;
     }
 
