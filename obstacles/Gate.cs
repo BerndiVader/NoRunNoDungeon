c759484237ab89ea5f489448098f9a72656aeb2b
BerndiVader <kasermandel@gmail.com>
2026-09-26 19:23:27 +0200

    Minor adjustments

--- obstacles/Gate.cs
+++ obstacles/Gate.cs
@@ -104,7 +104,6 @@ public class Gate : Area2D,ISwitchable
                 {
                     settings=new Settings(World.level,Vector2.Zero,(float)LEVEL_SETTINGS["Speed"],(float)LEVEL_SETTINGS["Zoom"]);
                     settings.autoRestore=World.level.settings.autoRestore;
-                    settings.restoreToDefault=false;
                     settings.Set();
                 }
 
